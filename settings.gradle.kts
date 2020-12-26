f22576ebce02187eddb6a5aba1357b8efdb4642e
Luteh <luthfanmaftuh@gmail.com>
2020-12-26 11:17:02 +0700

    Resolve modules implementation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,11 @@
 rootProject.name = "Movie App"
+
+// Accessing definitions from buildSrc in settings scripts was deprecated in Gradle 5.6.
+
+// Previously, the buildSrc project was built before applying the project’s settings script and its classes
+// were visible within the script. Now, buildSrc is built after the settings script and its classes are not
+// visible to it. The buildSrc classes remain visible to project build scripts and script plugins.
+
 include(
     ":app",
     ":core",
@@ -7,5 +14,5 @@ include(
     ":features:discover",
     ":features:genres",
     ":features:trailerVideo",
-    ":features:userReview"
+    ":features:userReviews"
 )
